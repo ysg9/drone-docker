c260f6f2673de301f4b70d6adb308872d15befeb
yungsg <ysg@nus.edu.sg>
2022-05-21 18:29:32 +0800

    update

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -x
 
-# update deps
-go mod tidy
+# to fix CVE-2019-11840
+go get golang.org/x/crypto
 
-go build -v -a -tags netgo -o release/linux/amd64/drone-docker ./cmd/drone-docker
+go mod tidy
+go build -v -a -tags netgo -trimpath -o release/linux/amd64/drone-docker ./cmd/drone-docker
 ls -l release/linux/amd64/drone-docker
 
 # for deps scanning
